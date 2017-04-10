84ab94992b185d36d0c2aace73c5163f74474af6
Florian Faissole <florianfaissole@gmail.com>
2017-04-10 19:07:26 +0200

    remove lic

--- finitedim_dec.v
+++ finitedim_dec.v
@@ -1,17 +1,3 @@
-(**
-
-Copyright (C) Faissole
-
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 3 of the License, or (at your option) any later version.
-
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-COPYING file for more details.
-*)
 
 
 (** * Finite dimensional subspaces of Hilbert spaces are closed: 
